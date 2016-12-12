c48daa0f8ba333eb480cfb357c363b71376f5876
wqqsukida <wuyifei19881027@163.com>
2016-12-12 16:08:40 +0800

    2nd commit

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding:utf-8
+print 'Hello World'
+from multiprocessing import Pool
+import time
+
+def f(x):
+	time.sleep(2)
+	return x*x
+
+if __name__ == '__main__':
+	p = Pool(5)
+	print p.map(f,[1,2,3,4,5])
